01acbf2bf9b38278eb25c5333f99068f593cb8cf
J0na17anT98 <pilotj@outlook.com>
2016-06-15 14:14:28 -0400

    web request son formatted
    learn how to get JSON format from website link

--- webrequest-test/ViewController.swift
+++ webrequest-test/ViewController.swift
@@ -12,12 +12,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        let urlString = "http://swapi.co/api/people/1/"
+        let session = NSURLSession.sharedSession()
+        let url = NSURL(string: urlString)!
+        
+        session.dataTaskWithURL(url) { (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+            
+            if let responseData = data {
+                do {
+                    let json = try NSJSONSerialization.JSONObjectWithData(responseData, options: NSJSONReadingOptions.AllowFragments)
+                    
+                    print(json)
+                } catch {
+                    print("Could not Serialize")
+                }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+            }
+        }.resume()
+    
     }
 
 
